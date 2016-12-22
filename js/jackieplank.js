1bce686e5f763b5e52bb731197aa675c6bc60a33
Eric Davis <davis.25811@gmail.com>
2016-12-22 13:56:48 -0500

    Added accordian tabs to FAQ and started gallery

---
+++ js/jackieplank.js
@@ -0,0 +1,8 @@
+/*
+ * Switch Plus/Minus on collapsable panels
+ */
+$('.collapse').on('shown.bs.collapse', function(){
+$(this).parent().find(".glyphicon-plus").removeClass("glyphicon-plus").addClass("glyphicon-minus");
+}).on('hidden.bs.collapse', function(){
+$(this).parent().find(".glyphicon-minus").removeClass("glyphicon-minus").addClass("glyphicon-plus");
+});
