d050f25a0d3eff747c5dd2573ab7d9b5c6ddc640
Eric Davis <davis.25811@gmail.com>
2016-12-23 19:28:45 -0500

    Added galleries to Listings page and cleaned up some code.

--- js/gallery-carousel.js
+++ js/gallery-carousel.js
@@ -1,27 +1,28 @@
 /*
  * Carousel Gallery JavaScript snippet
- * Taken From: http://bootsnipp.com/snippets/featured/carousel-with-gallery-thumbs
+ * Takem From: http://bootsnipp.com/snippets/featured/carousel-extended#comments
+ * 
+ * NOTE: CAROUSEL ON TOP OF THUMBNAILS
  */
-jQuery(document).ready(function($) {
-
-	$('#myCarousel').carousel({
-		interval : 5000
-	});
-
-	//Handles the carousel thumbnails
-	$('[id^=carousel-selector-]').click(function() {
-		var id_selector = $(this).attr("id");
-		try {
-			var id = /-(\d+)$/.exec(id_selector)[1];
-			console.log(id_selector, id);
-			jQuery('#myCarousel').carousel(parseInt(id));
-		} catch (e) {
-			console.log('Regex failed!', e);
-		}
-	});
-	// When the carousel slides, auto update the text
-	$('#myCarousel').on('slid.bs.carousel', function(e) {
-		var id = $('.item.active').data('slide-number');
-		$('#carousel-text').html($('#slide-content-' + id).html());
-	});
-}); 
+ jQuery(document).ready(function($) {
+ 		// Remove auto slide (set false to a number of milliseconds to enable)
+        $('#myCarousel').carousel({
+                interval: false
+        });
+ 
+        $('#carousel-text').html($('#slide-content-0').html());
+ 
+        //Handles the carousel thumbnails
+       $('[id^=carousel-selector-]').click( function(){
+            var id = this.id.substr(this.id.lastIndexOf("-") + 1);
+            var id = parseInt(id);
+            $('#myCarousel').carousel(id);
+        });
+ 
+ 
+        // When the carousel slides, auto update the text
+        $('#myCarousel').on('slid.bs.carousel', function (e) {
+                 var id = $('.item.active').data('slide-number');
+                $('#carousel-text').html($('#slide-content-'+id).html());
+        });
+});
--- js/jackieplank.js
+++ js/jackieplank.js
@@ -6,3 +6,23 @@ $(this).parent().find(".glyphicon-plus").removeClass("glyphicon-plus").addClass(
 }).on('hidden.bs.collapse', function(){
 $(this).parent().find(".glyphicon-minus").removeClass("glyphicon-minus").addClass("glyphicon-plus");
 });
+
+
+/*
+ * Resizable & Draggable Modals
+ */
+// Add "modal-resizable" class to the Bootstap "modal-content" div class property
+$('.modal-resizable').resizable
+({
+	//alsoResize: ".modal-dialog",
+		minHeight : 300,
+		minWidth : 300
+});
+
+$('.modal-dialog').draggable();
+
+// Test this for removal
+$('#listings-modal').on('show.bs.modal', function() 
+{
+	$(this).find('.modal-body').css({'max-height' : '100%'});
+});
